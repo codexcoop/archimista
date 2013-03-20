d183c3f491c6dadb4ad8bea00a8ffffe6adb1916
Cristiano Animosi <cristiano@codexcoop.it>
2013-03-20 20:51:14 +0100

    Changelog 1.2.0 rc2

--- config/initializers/metadata.rb
+++ config/initializers/metadata.rb
@@ -1,7 +1,7 @@
 # Application metadata
 APP_NAME = "Archimista"
 APP_VERSION = "1.2.0"
-APP_STATUS = "rc1"
+APP_STATUS = "rc2"
 AEF_COMPATIBLE_VERSIONS = [110, 120]
 APP_CREATOR = "Codex Società Cooperativa - Pavia"
 APP_EDITION = "server" # server | standalone | hub
